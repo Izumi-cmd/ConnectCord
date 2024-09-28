ffca7d7688e9897e5948e257bd04d5fce74e7276
Izumi_cmd <ryutaro.jp@gmail.com>
2024-09-29 00:47:21 +0900

    :fix:Profile画面の新規作成

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ProfileForm from "./components/ProfileForm";
+import "./App.css";
+import "./reset.css";
 
 function App() {
+  const [profile, setProfile] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const [qrCode, setQrCode] = useState(null);
+
+  const handleSubmit = (profileData) => {
+    setProfile(profileData);
+    // QRコードを生成する処理を呼び出す.（JSON文字列を使用）
+    setQrCode(JSON.stringify(profileData));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">ConnectCord</h1>
+      <div>
+        <ProfileForm onSubmit={handleSubmit} />
+      </div>
+      {/* <div>
+        <QRcodeDisplay data={qrCode} />
+      </div> */}
     </div>
   );
 }
